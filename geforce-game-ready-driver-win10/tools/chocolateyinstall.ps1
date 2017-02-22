4f5c8fd57e9e9f6b689c8c89188fa7e3f743fde5
Archimedes Trajano <arch@trajano.net>
2017-02-22 18:12:08 -0500

    Geforce Game Ready Driver for Windows 10 378.66
    This is a reimplementation of the existing
    geforce-game-ready-driver-win10.  It disables the following components
    from the installation to prevent any updates from being performed by
    the NVidia software.
    
    * GEForce Experience
    * NVidia Update
    
    In addition the following programs are also removed to reduce the bloat
    of components being installed.
    
    * 3D Vision
    * NView

---
+++ geforce-game-ready-driver-win10/tools/chocolateyinstall.ps1
@@ -0,0 +1,47 @@
+﻿$installerFile = (Join-Path ${ENV:TEMP} "nvidiadriver.exe")
+$workDirectory = New-Item (Join-Path ${ENV:TEMP} "nvidiadriver") -ItemType Directory -Force
+
+$packageArgs = @{
+    packageName   = $env:chocolateyPackageName
+    url = "http://us.download.nvidia.com/Windows/$env:chocolateyPackageVersion/$env:chocolateyPackageVersion-desktop-win10-32bit-international-whql.exe"
+    checksum      = 'ea988083e3b43cbcbcef54ad29b6d8d2c4f625460de41292c48661354837e6f0'
+    checksumType  = 'sha256'
+
+    url64bit = "http://us.download.nvidia.com/Windows/$env:chocolateyPackageVersion/$env:chocolateyPackageVersion-desktop-win10-64bit-international-whql.exe"
+    checksum64    = 'ddf2e726eefd1e5cdaba31ec023c075df282685377652c1bdd343be1b1792b9cc393f0eed50b0ebc31b37bd651470f86a908de559c3969ea7472e5b0045ac12c'
+    checksumType64= 'sha512'
+
+    silentArgs = "-s -noreboot -clean" 
+}
+
+# TODO make this part driven by parameters to enable or disable components should people want GEForce Experience or 3DVision
+$deselectedPatterns = @(
+    "NV3DVision*", 
+    "GFExperience*",
+    "Display.NView", 
+    "Display.Update")
+
+Get-ChocolateyWebFile @packageArgs -FileFullPath $installerFile
+Get-ChocolateyUnzip $installerFile $workDirectory
+
+# Update the setup.cfg
+$setupCfgFile = Join-Path $workDirectory "setup.cfg"
+[xml] $setupCfgDoc = (Get-Content $setupCfgFile)
+
+# Explicitly deselect packages
+foreach ($pattern in $deselectedPatterns) {
+    $child = $setupCfgDoc.CreateElement("deselect")
+    $childAttribute = $setupCfgDoc.CreateAttribute("name")
+    $childAttribute.Value = "**\$pattern\**\*"
+    [void] $child.Attributes.Append($childAttribute)
+    [void] $setupCfgDoc.setup.install.search.AppendChild($child)
+}
+($setupCfgDoc.setup.install.'sub-package' | where { $_.name -eq 'Display.GFExperience' }).disposition = 'demand'
+($setupCfgDoc.setup.install.'sub-package' | where { $_.name -eq 'Display.Update' }).disposition = 'demand'
+
+[void] $setupCfgDoc.Save($setupCfgFile)
+
+Install-ChocolateyInstallPackage @packageArgs -File (Join-Path $workDirectory "setup.exe")
+
+Remove-Item $workDirectory -Recurse -Force
+Remove-Item $installerFile -Force
