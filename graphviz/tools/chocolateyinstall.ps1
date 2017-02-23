a2fe0030c13bf636bd0450fd5bf1250b8fc90a46
Archimedes Trajano <developer@trajano.net>
2017-02-23 10:38:03 -0500

    GraphViz 2.38
    Unlike the official package, this uses the ZIP version of the installer
    and utliizes the shims created by Chocolatey for the binaries.

---
+++ graphviz/tools/chocolateyinstall.ps1
@@ -0,0 +1,11 @@
+﻿# Just extract to the package folder because the zip file will have everything inside "release" directory.
+$packageArgs = @{
+  packageName   = $env:chocolateyPackageName
+  unzipLocation = $env:chocolateyPackageFolder
+
+  url           = 'http://www.graphviz.org/pub/graphviz/stable/windows/graphviz-2.38.zip'
+  checksum      = '660d7e11cfac073b396ecca46f3bbabda123d84735b83b0f526896053c2a9f0254aac26691b85b37a22ba50868e1c626c7ffa7489d15ac32af5e37a0c71e3869'
+  checksumType  = 'sha512'
+}
+
+Install-ChocolateyZipPackage @packageArgs
