260683bf7198f4dc39b27a6096f795c6fe37be7d
Archimedes Trajano <developer@trajano.net>
2017-02-22 22:13:50 -0500

    Strings 2.53

---
+++ strings/tools/chocolateyinstall.ps1
@@ -0,0 +1,20 @@
+﻿# Just extract to the package folder there is only one file because the 32-bit one will be removed if we are on a 64-bit machine
+$packageArgs = @{
+    packageName   = $env:chocolateyPackageName
+    unzipLocation = $env:chocolateyPackageFolder
+
+    url           = 'https://download.sysinternals.com/files/Strings.zip'
+    checksum      = ''
+    checksumType  = 'sha512'
+}
+
+Install-ChocolateyZipPackage @packageArgs
+
+if (Get-OSArchitectureWidth -Compare 32) {
+    Remove-Item (Join-Path $env:chocolateyPackageFolder 'strings64.exe')
+}
+
+if (Get-OSArchitectureWidth -Compare 64) {
+    Remove-Item (Join-Path $env:chocolateyPackageFolder 'strings.exe')
+    Rename-Item (Join-Path $env:chocolateyPackageFolder "strings64.exe") "strings.exe"
+}
