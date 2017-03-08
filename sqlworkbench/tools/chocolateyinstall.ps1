dbc87a5e8d0a06157f685a6a723982321c6e9b33
Archimedes Trajano <developer@trajano.net>
2017-03-08 11:18:14 -0500

    SQLWorkbench/J Build 122

---
+++ sqlworkbench/tools/chocolateyinstall.ps1
@@ -0,0 +1,27 @@
+﻿. "$env:chocolateyPackageFolder/tools/globals.ps1"
+
+$packageArgs = @{
+  packageName   = $env:chocolateyPackageName
+  unzipLocation = $env:chocolateyPackageFolder
+
+  url           = 'http://www.sql-workbench.net/Workbench-Build${env:chocoPackageVersion}.zip'
+  checksum      = ''
+  checksumType  = 'sha512'
+}
+
+Install-ChocolateyZipPackage @packageArgs
+
+# Only keep the one for the architecture needed
+if (Get-OSArchitectureWidth -Compare 32) {
+    Remove-Item (Join-Path $env:chocolateyPackageFolder 'SQLWorkbench64.exe')
+    Remove-Item (Join-Path $env:chocolateyPackageFolder 'sqlwbconsole64.exe')
+}
+
+if (Get-OSArchitectureWidth -Compare 64) {
+    Remove-Item (Join-Path $env:chocolateyPackageFolder 'sqlwbconsole.exe')
+    Rename-Item (Join-Path $env:chocolateyPackageFolder "sqlwbconsole64.exe") "sqlwbconsole.exe"
+    Remove-Item (Join-Path $env:chocolateyPackageFolder 'SQLWorkbench.exe')
+    Rename-Item (Join-Path $env:chocolateyPackageFolder "SQLWorkbench64.exe") "SQLWorkbench.exe"
+}
+
+Install-ChocolateyShortcut  -ShortcutFilePath $shortcut -TargetPath (Join-Path $env:chocolateyPackageFolder 'SQLWorkbench.exe') -PinToTaskbar
