f114bc38d1491794c9636f79c0ac20ef8fafb204
Archimedes Trajano <developer@trajano.net>
2017-03-22 22:57:28 -0400

    Corsair CUE2 2.11.115

---
+++ corsair-cue2/tools/chocolateyinstall.ps1
@@ -0,0 +1,12 @@
+﻿$packageArgs = @{
+    packageName   = $env:chocolateyPackageName
+    url = "https://downloads.corsair.com/download?item=Files/CUE/CorsairUtilityEngineSetup_${env:chocolateyPackageVersion}_release.msi"
+    fileType = 'msi'
+    checksum      = ''
+    checksumType  = 'sha256'
+    silentArgs = "/QUIET /NORESTART"
+}
+Install-ChocolateyPackage @packageArgs
+
+# Start CUE2 since it won't automatically start up after install.
+Start-Process (Join-Path (Get-AppInstallLocation "Corsair Utility Engine") "CUE.exe") -WindowStyle Hidden
