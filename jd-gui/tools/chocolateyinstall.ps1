b6312a59998ff9e28f203793442326e6894a8e4a
Archimedes Trajano <developer@trajano.net>
2017-03-24 21:12:26 -0400

    JD-GUI 1.4.0

---
+++ jd-gui/tools/chocolateyinstall.ps1
@@ -0,0 +1,14 @@
+﻿. "$env:chocolateyPackageFolder/tools/globals.ps1"
+
+$packageArgs = @{
+  packageName   = $env:chocolateyPackageName
+  unzipLocation = $env:chocolateyPackageFolder
+
+  url           = "https://github.com/java-decompiler/jd-gui/releases/download/v${env:chocolateyPackageVersion}/jd-gui-windows-${env:chocolateyPackageVersion}.zip"
+  checksum      = ''
+  checksumType  = 'sha512'
+}
+
+Install-ChocolateyZipPackage @packageArgs
+
+Install-ChocolateyShortcut  -ShortcutFilePath $shortcut -TargetPath (Join-Path $env:chocolateyPackageFolder 'jd-gui.exe') -PinToTaskbar
